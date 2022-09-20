e2846d837c9951062019ebb454419b84eb5ade95
Priyanshu Patel <95230102+Priyanshu2410@users.noreply.github.com>
2022-09-20 15:48:19 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+// ReactDOM.render(<h1>hello priyanshu</h1>, document.getElementById("root"))
+
+// ReactDOM.render(<ul><li>priyanshu</li><li>om</li></ul>, document.getElementById("root"))
+function Mainfunction() {
+    return(
+        <h1>hello world</h1>
+    )
+}
+
+ReactDOM.render(
+    <div>
+        <Mainfunction/>
+    </div>,
+    document.getElementById("root")
+)

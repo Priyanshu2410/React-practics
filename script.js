3f64f72c1e0964efb6aeebe2e9bbb9b67ec66bd6
Priyanshu Patel <95230102+Priyanshu2410@users.noreply.github.com>
2022-09-24 22:00:03 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+
+
+
 // ReactDOM.render(<h1 className="header" >hello priyanshu</h1>, document.getElementById("root"))
 
 // ReactDOM.render(<ul><li>priyanshu</li><li>om</li></ul>, document.getElementById("root"))
@@ -17,14 +20,78 @@
 //     document.getElementById("root")
 // )
 
+
+// const myelement = (
+//     <table>
+//       <tr>
+//         <th>Name</th>
+//       </tr>
+//       <tr>
+//         <td>John</td>
+//       </tr>
+//       <tr>
+//         <td>Elsa</td>
+//       </tr>
+//     </table>
+//   );
+  
+//   ReactDOM.render(myelement, document.getElementById('root'));
+  
+  
+
 // simple java script 
 // const h1= document.createElement("h1")
 // h1.textContent="this is priyanshu"
 // h1.className = "header"
 // document.getElementById("root").append(h1)
 
-const element = <h1 className="header">this is priyanshu</h1>
-console.log(element)
+// const element = <h1 className="header">this is priyanshu</h1>
+// console.log(element)
 
-//jsx
-ReactDOM.render(element,document.getElementById("root"))
+// //jsx
+// ReactDOM.render(element,document.getElementById("root"))
+
+// const page =(
+//     <div>
+//         <h1 className="header">this is prishu</h1>
+//         <p>this is legend</p>
+//     </div>
+// )
+// ReactDom.render(
+//     page,
+//     document.getElementById("root")
+// )
+
+
+// const navbar = (
+//     <nav>
+//         <h1>this is hotel</h1>
+//         <ul>
+//             <li>Menu</li>
+//             <li>about</li>
+//             <li>contect</li>
+//         </ul>
+//     </nav>
+// )
+
+// ReactDOM.rend>er(navbar, document.getElementById('root'));
+
+
+
+// import React from "react"
+// import ReactDOM from "react-dom"
+
+
+const page =(
+    <div>
+        <img src="./wallpaper2.png" width="40px"/>
+        <h1>this is Prishu</h1>
+        <ul>
+            <li>this is first</li>
+            <li>this is second</li>
+            <li>this is third</li>
+        </ul>
+    </div>
+)
+
+ReactDOM.render(page,document.getElementById('root'))

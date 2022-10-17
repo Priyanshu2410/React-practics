e0b53e4290b6aae8093d347edc19020766395662
Priyanshu Patel <95230102+Priyanshu2410@users.noreply.github.com>
2022-10-17 19:13:57 +0530

    Add files via upload

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -2,14 +2,14 @@ import React from 'react';
 import './App.css';
 import Navbar from "./components/navbar";
 import Maincont from "./components/main";
-
-
+import Card from "./components/Card";
 
 function App() {
   return (
     <>
       <Navbar />
       <Maincont />
+      <Card />
     </>
   );
 }
--- my-app/src/components/navbar.js
+++ my-app/src/components/navbar.js
@@ -1,5 +1,10 @@
 import React from "react"
 
 export default function Navbar(){
-    return <h1>navbar components</h1>
+    return (
+        <nav>
+           <img src="https://tse4.mm.bing.net/th?id=OIP.0ku1AvUo__-ohxH4EqTXOAHaEo&pid=Api&P=0" alt="logo" className="nav-logo" />
+            
+        </nav>
+    )
 }
